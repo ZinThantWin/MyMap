4042a69192a714f3fcd6115fe230af84cb0b2094
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-27 15:34:45 +0630

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by ကင်ဇို on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
